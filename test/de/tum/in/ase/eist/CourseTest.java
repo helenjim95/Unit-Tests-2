ca18572a49b1c4d431e0527b1cd9416fb6cca3b3
Lucas Welscher <lucas.welscher@tum.de>
2022-05-15 22:04:36 +0200

    Exercise-Template pushed by Artemis

---
+++ test/de/tum/in/ase/eist/CourseTest.java
@@ -0,0 +1,13 @@
+package de.tum.in.ase.eist;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseTest {
+
+	// TODO 1: Test getCourseTitle()
+
+	// TODO 2: Test getNumberOfAttendees()
+
+}
---
+++ test/de/tum/in/ase/eist/OnlineCourseTest.java
@@ -0,0 +1,10 @@
+package de.tum.in.ase.eist;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OnlineCourseTest {
+
+	// TODO 3: Test setOnlineCourseUrl()
+}
