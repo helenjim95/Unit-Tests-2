5fa1b4b8d885716bdf799b123b94df609f83fbbb
helenjim49 <helenjim49@gmail.com>
2022-06-21 21:29:34 +0200

    third

--- test/de/tum/in/ase/eist/OnlineCourseTest.java
+++ test/de/tum/in/ase/eist/OnlineCourseTest.java
@@ -13,6 +13,7 @@ class OnlineCourseTest {
     @Test
     void testSetOnlineCourseUrlWithValidUrl() throws MalformedURLException {
         OnlineCourse onlineCourse = new OnlineCourse("Netflix");
+        onlineCourse.setUrl("www.netflix.com");
         assertThrows(MalformedURLException.class, () -> { onlineCourse.setUrl("www.netflix.com"); });
         URL expected = new URL("https://www.netflix.com/");
         URL observed = onlineCourse.getUrl();
