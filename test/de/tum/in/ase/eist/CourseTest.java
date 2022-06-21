13ff3f42e7d3ff24e32d84ed6eb9d26f8c98f84e
helenjim49 <helenjim49@gmail.com>
2022-06-21 19:32:01 +0200

    init

--- test/de/tum/in/ase/eist/CourseTest.java
+++ test/de/tum/in/ase/eist/CourseTest.java
@@ -2,12 +2,41 @@ package de.tum.in.ase.eist;
 
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CourseTest {
 
-	// TODO 1: Test getCourseTitle()
 
-	// TODO 2: Test getNumberOfAttendees()
 
+	// 1: Test getCourseTitle()
+    @Test
+    void testGetCourseTitle() {
+        Course course1 = new Course("Math");
+        String expected = "Math";
+        String observed = course1.getTitle();
+        assertEquals(expected, observed);
+    }
+
+
+	// 2: Test getNumberOfAttendees()
+    @Test
+    void testNoAttendees() {
+        Course course1 = new Course("Math");
+        int expected = 0;
+        int observed = course1.getNumberOfAttendees();
+        assertEquals(expected, observed);
+    }
+
+    void testThreeAttendees() {
+        Course course1 = new Course("Math");
+        Student Ed = new Student("Ed", "Sheeran", "10/18/1990", "Math",  "Guitar");
+        Student Charlie = new Student("Charlie", "Puth", "02/21/2007", "Math",  "Piano");
+        Student Shawn = new Student("Shawn", "Mendes", "02/21/1991", "Math",  "Vocal");
+        course1.addAttendee(Ed);
+        course1.addAttendee(Charlie);
+        course1.addAttendee(Shawn);
+        int expected = 3;
+        int observed = course1.getNumberOfAttendees();
+        assertEquals(expected, observed);
+    }
 }
--- test/de/tum/in/ase/eist/OnlineCourseTest.java
+++ test/de/tum/in/ase/eist/OnlineCourseTest.java
@@ -2,9 +2,24 @@ package de.tum.in.ase.eist;
 
 import org.junit.jupiter.api.*;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class OnlineCourseTest {
 
-	// TODO 3: Test setOnlineCourseUrl()
+	// 3: Test setOnlineCourseUrl()
+    @Test
+    void testSetOnlineCourseUrlWithValidUrl() throws MalformedURLException {
+        OnlineCourse onlineCourse = new OnlineCourse("Netflix");
+        onlineCourse.setUrl("www.netflix.com");
+        URL expected = new URL("www.netflix.com");
+        URL observed = onlineCourse.getUrl();
+        assertEquals(expected, observed);
+    }
+    void testSetOnlineCourseUrlWithInvalidUrl() throws MalformedURLException {
+        OnlineCourse onlineCourse = new OnlineCourse("Netflix");
+        assertThrows(MalformedURLException.class, () -> { onlineCourse.setUrl("www.netflix.comdsdsd"); });
+    }
 }
